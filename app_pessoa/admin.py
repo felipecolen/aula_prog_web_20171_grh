c0292416d32f92f16257d3b56abc71bcb822bd2e
Professor <Professor>
2017-02-15 22:19:36 -0300

    add admin 2a aula

--- app_pessoa/admin.py
+++ app_pessoa/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from app_pessoa.models import Pessoa
+
+
+class PessoaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'cpf', 'sexo')
+
+admin.site.register(Pessoa, PessoaAdmin)
